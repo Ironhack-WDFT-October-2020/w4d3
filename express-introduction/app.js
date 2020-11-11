656a196289c07e2519d22192178b39f5b01a084c
Jan Redmann <janredmann1@gmail.com>
2020-11-11 15:16:25 +0100

    update express-intro

--- express-introduction/app.js
+++ express-introduction/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
+// this makes the public folder available in the html files
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (request, response) => {
     console.log('this is the home page!!');
     // response.send('<h1>Hello from Express</h1>')
-    response.sendFile(__dirname__ + '/views/home-page.html');
+    response.sendFile(__dirname + '/views/home-page.html');
+});
+
+app.get('/dog', (req, res) => {
+    res.sendFile(__dirname + '/views/dog.html');
 });
 
 app.listen(3000, () => {
